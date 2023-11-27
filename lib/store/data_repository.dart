0ae7e1e119b553055685f9479d0743f7dcc0f8a4
Luis Felipe <luisfelipe.santos@aegro.com.br>
2023-11-26 22:18:45 -0300

    refactor: change data structure
    Also, remove unnecessary method

--- lib/store/data_repository.dart
+++ lib/store/data_repository.dart
@@ -1,11 +1,7 @@
 abstract interface class DataRepository {
-  Future<List<String?>?> retrieveData();
-
-  Future<List<String>?> retrieveKeys();
+  Future<Map<String, String?>?> retrieveData();
 
   Future<void> save(final String key, final String data);
 
   Future<void> erase(final String key);
-
-  Future<void> eraseAll();
 }
--- lib/store/standard_data_repository.dart
+++ lib/store/standard_data_repository.dart
@@ -1,45 +1,31 @@
+import 'package:collection/collection.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'data_repository.dart';
 
 class StandardDataRepository implements DataRepository {
   @override
-  Future<List<String?>?> retrieveData() async {
+  Future<Map<String, String?>?> retrieveData() async {
     final sharedPreferences = await SharedPreferences.getInstance();
-    final allKeys = sharedPreferences.getKeys();
-    final List<String?> allData = [];
+    final allKeys = sharedPreferences.getKeys().sorted();
+    final Map<String, String?> allData = {};
 
     for (final key in allKeys) {
-      allData.add(sharedPreferences.getString(key));
+      allData.addAll({key: sharedPreferences.getString(key)});
     }
 
     return allData.isNotEmpty ? allData : null;
   }
 
   @override
-  Future<List<String>?> retrieveKeys() async {
-    final sharedPreferences = await SharedPreferences.getInstance();
-
-    final keysList = sharedPreferences.getKeys().toList();
-    return keysList.isNotEmpty ? keysList : null;
-  }
-
-  @override
   Future<void> save(String key, String data) async {
     final sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.setString(key, data);
-    await sharedPreferences.reload();
   }
 
   @override
   Future<void> erase(String key) async {
     final sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.remove(key);
-  }
-
-  @override
-  Future<void> eraseAll() async {
-    final shared = await SharedPreferences.getInstance();
-    await shared.clear();
   }
 }
